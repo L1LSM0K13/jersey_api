33e247c22254b65d5d2b7d59ca5e1fbb5cbc60a9
Omar <omarmares@purelymail.com>
2024-07-15 21:13:44 -0500

    base api set up

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+// Config
+require('dotenv').config({path:'./config/.env'});
+const uri = process.env.MONGODB_URI;
+// Imports
 const express = require('express');
 const app = express();
 const PORT  = process.env.PORT || 8080;
 
-app.listen(PORT,() => console.log(`Listening on ${PORT}`));
+// MongoDB Connection
+const mongoose = require('mongoose');
+const playerRoute = require('./routes/player.route.js');
 
+// Port listening on 8080 and connecting to mongoDB
+mongoose.connect(uri)
+    .then(() => { app.listen(PORT,() => console.log(`It's live on http://localhost:${PORT}/api/player_numbers`));
+    console.log("connected to MongoDB...")
+}).catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('');
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Routes
+app.use('/api/player_numbers', playerRoute);
+
+// Endpoints
+app.get('/', async (req, res) => {
+    res.status(200).send({});
 })
